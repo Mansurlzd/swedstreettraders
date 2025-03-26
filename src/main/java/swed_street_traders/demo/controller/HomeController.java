36cae9e2d3090706d6b1200f5d472d795118549a
lzd <mansur.alizada@gmail.com>
2025-03-26 11:42:26 +0200

    first commit

---
+++ src/main/java/swed_street_traders/demo/controller/HomeController.java
@@ -0,0 +1,15 @@
+package swed_street_traders.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "Welcome to our Spring Boot application!");
+        return "index";
+    }
+}
